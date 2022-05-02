bf8a14c6e18448f03cc1ea023d066c8cae1ff3cb
wonseok <tkddls4456@gmail.com>
2022-05-03 02:17:05 +0900

    rename lat,long type

--- app/src/main/java/com/example/graduation_android/locationdata/LocationResponse.java
+++ app/src/main/java/com/example/graduation_android/locationdata/LocationResponse.java
@@ -2,15 +2,17 @@ package com.example.graduation_android.locationdata;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
+
 /* 서버에서 받을 위도, 경도들 */
 public class LocationResponse {
     @SerializedName("latitude")
-    private Double latitude;
+    private ArrayList<String> latitude = new ArrayList<>();
 
     @SerializedName("longitude")
-    private Double longitude;
+    private ArrayList<String> longitude = new ArrayList<>();
 
-    public Double getLatitude() { return latitude; }
+    public ArrayList getLatitude() { return latitude; }
 
-    public Double getLongitude() { return longitude; }
+    public ArrayList getLongitude() { return longitude; }
 }
