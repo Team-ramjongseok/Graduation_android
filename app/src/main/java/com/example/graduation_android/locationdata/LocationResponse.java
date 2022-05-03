012a02ab13ef52369a12b4a18af8e0e913dba0f4
wonseok <tkddls4456@gmail.com>
2022-05-03 22:38:50 +0900

    [feat] main_gps_distanceResult

--- app/src/main/java/com/example/graduation_android/locationdata/LocationResponse.java
+++ app/src/main/java/com/example/graduation_android/locationdata/LocationResponse.java
@@ -6,13 +6,28 @@ import java.util.ArrayList;
 
 /* 서버에서 받을 위도, 경도들 */
 public class LocationResponse {
+
     @SerializedName("latitude")
-    private ArrayList<String> latitude = new ArrayList<>();
+    private ArrayList<Double> latitude = new ArrayList<>();
 
     @SerializedName("longitude")
-    private ArrayList<String> longitude = new ArrayList<>();
+    private ArrayList<Double> longitude = new ArrayList<>();
+
+    @SerializedName("distanceResult")
+    private ArrayList<Cafe> distanceResult = new ArrayList<>();
 
     public ArrayList getLatitude() { return latitude; }
 
     public ArrayList getLongitude() { return longitude; }
+    public ArrayList getCafe() {return distanceResult;}
+}
+
+class Cafe {
+    public String cafe_info;
+    public String location;
+    public int seat_empty;
+    public int seat_all;
+    public Double latitude;
+    public Double longitude;
+    public Double distance;
 }
