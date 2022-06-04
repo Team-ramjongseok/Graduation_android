97f32fd32b48992b846e22ccb94614fd841788d4
msdio <rlagofka1@naver.com>
2022-06-04 20:28:55 +0900

    [fix] handle in case paymentList is empty

--- app/src/main/java/com/example/graduation_android/Constants.java
+++ app/src/main/java/com/example/graduation_android/Constants.java
@@ -1,5 +1,6 @@
 package com.example.graduation_android;
 
 public class Constants {
-    public static final String URL = "http://3.38.128.16:8001/"; //사용할 ip 주소
+    public static final String URL = "http://3.38.128.16:8001/"; //사용할 서버 ip 주소
+    public static final String URL_LOCAL = "http://10.0.2.2:8001/"; //로컬 ip 주소
 }
