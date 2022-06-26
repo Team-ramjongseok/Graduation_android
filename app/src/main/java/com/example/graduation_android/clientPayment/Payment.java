c9f101f4cd2ab1dd99fa1be0b1b62742b701f410
msdio <rlagofka1@naver.com>
2022-06-26 17:28:25 +0900

    [design] minor design fix

--- app/src/main/java/com/example/graduation_android/clientPayment/Payment.java
+++ app/src/main/java/com/example/graduation_android/clientPayment/Payment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.graduation_android.Constants;
